8c65e81268817fd68261a48303e1648cc7d3852d
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2025-09-19 16:49:24 +0800

    Add files via upload

---
+++ myNewspaper/writer/includes/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light p-4 shadow-sm" style="background-color: #ADEBB3;">
+  <div class="container">
+    <a class="navbar-brand font-weight-bold" href="index.php">Writer Panel</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav navbar-light ml-auto font-weight-bold">
+        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="articles_submitted.php">Articles Submitted</a></li>
+        <li class="nav-item"><a class="nav-link" href="shared_articles.php">Shared Articles</a></li>
+        <li class="nav-item"><a class="nav-link" href="core/handleForms.php?logoutUserBtn=1">Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
